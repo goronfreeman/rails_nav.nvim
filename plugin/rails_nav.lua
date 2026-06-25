01e9b2f06a170e213bfed9b3825a0ebd62d729d3
Hunter Braun <hunter.braun@gmail.com>
2026-06-25 14:19:27 -0500

    Change binding to <leader>rc

--- plugin/rails_nav.lua
+++ plugin/rails_nav.lua
@@ -13,7 +13,7 @@ vim.api.nvim_create_autocmd("User", {
 -- Fallback in case User event isn't fired by setup
 vim.schedule(function()
   pcall(function()
-    vim.keymap.set("n", "<leader>rn", function()
+    vim.keymap.set("n", "<leader>rc", function()
       require("telescope").extensions.rails_nav.rails_nav()
     end, { desc = "Rails Nav picker" })
   end)
